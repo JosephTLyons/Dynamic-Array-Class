3f3bc3589875fc1d1d4f2bb5626f4cebf7e21125
Joseph Lyons <joe_04_04@yahoo.com>
2016-08-04 07:14:01 -0400

    Initial Commit

---
+++ Grow and Shrink Class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Grow and Shrink Class
+//
+//  Created by Joseph Lyons on 8/4/16.
+//  Copyright © 2016 Joseph Lyons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
